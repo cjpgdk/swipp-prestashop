e0ad1b697eb176099dc720cc5b0ad6c705b0e69b
Christian M. Jensen <christian@cmjscripter.net>
2014-09-01 15:20:00 +0200

    version 1.4 PrestaShop 1.4 100% supported, added currency support with conversion to DKK

--- modules/swipp/controllers/front/validation.php
+++ modules/swipp/controllers/front/validation.php
@@ -1,63 +1,1 @@
-<?php
-
-/*
- * Copyright (C) 2014  Christian M. Jensen
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  @author Christian M. Jensen <christian@cmjscripter.net>
- *  @copyright 2014 Christian M. Jensen
- *  @license http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3
- */
-
-/**
- * @since 1.5.0
- */
-class SwippValidationModuleFrontController extends ModuleFrontController
-{
-	/**
-	 * @see FrontController::postProcess()
-	 */
-	public function postProcess()
-	{
-		$cart = $this->context->cart;
-		if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)
-			Tools::redirect('index.php?controller=order&step=1');
-
-		// Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
-		$authorized = false;
-		foreach (Module::getPaymentModules() as $module)
-			if ($module['name'] == 'swipp')
-			{
-				$authorized = true;
-				break;
-			}
-		if (!$authorized)
-			die($this->module->l('This payment method is not available.', 'validation'));
-
-		$customer = new Customer($cart->id_customer);
-		if (!Validate::isLoadedObject($customer))
-			Tools::redirect('index.php?controller=order&step=1');
-
-		$currency = $this->context->currency;
-		$total = (float)$cart->getOrderTotal(true, Cart::BOTH);
-		$mailVars = array(
-			'{swipp_phone}' => Configuration::get('SWIPP_PHONE'),
-			'{swipp_owner}' => Configuration::get('SWIPP_OWNER'),
-		);
-
-		$this->module->validateOrder($cart->id, Configuration::get('SWIPP_PAYMENT_STATE'), $total, $this->module->displayName, NULL, $mailVars, (int)$currency->id, false, $customer->secure_key);
-		Tools::redirect('index.php?controller=order-confirmation&id_cart='.$cart->id.'&id_module='.$this->module->id.'&id_order='.$this->module->currentOrder.'&key='.$customer->secure_key);
-	}
-}
+<?php/* * Copyright (C) 2014  Christian M. Jensen *  * This program is free software: you can redistribute it and/or modify * it under the terms of the GNU General Public License as published by * the Free Software Foundation, either version 3 of the License, or * (at your option) any later version. *  * This program is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the * GNU General Public License for more details. *  * You should have received a copy of the GNU General Public License * along with this program.  If not, see <http://www.gnu.org/licenses/>. * *  @author Christian M. Jensen <christian@cmjscripter.net> *  @copyright 2014 Christian M. Jensen *  @license http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 *//** * @since 1.5.0 */class SwippValidationModuleFrontController extends ModuleFrontController{	/**	 * @see FrontController::postProcess()	 */	public function postProcess()	{		$cart = $this->context->cart;		if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active)			Tools::redirect('index.php?controller=order&step=1');		// Check that this payment option is still available in case the customer changed his address just before the end of the checkout process		$authorized = false;		foreach (Module::getPaymentModules() as $module)			if ($module['name'] == 'swipp')			{				$authorized = true;				break;			}		if (!$authorized)			die($this->module->l('This payment method is not available.', 'validation'));		$customer = new Customer($cart->id_customer);		if (!Validate::isLoadedObject($customer))			Tools::redirect('index.php?controller=order&step=1');		$currency = $this->context->currency;		$total = (float)$cart->getOrderTotal(true, Cart::BOTH);		$mailVars = array(			'{swipp_phone}' => Configuration::get('SWIPP_PHONE'),			'{swipp_owner}' => Configuration::get('SWIPP_OWNER'),		);		$this->module->validateOrder($cart->id, Configuration::get('SWIPP_PAYMENT_STATE'), $total, $this->module->displayName, NULL, $mailVars, (int)$currency->id, false, $customer->secure_key);		Tools::redirect('index.php?controller=order-confirmation&id_cart='.$cart->id.'&id_module='.$this->module->id.'&id_order='.$this->module->currentOrder.'&key='.$customer->secure_key);	}}
---
+++ modules/swipp/payment_execution.tpl
@@ -0,0 +1,63 @@
+{*
+* Copyright (C) 2014  Christian M. Jensen
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*  @author Christian M. Jensen <christian@cmjscripter.net>
+*  @copyright 2014 Christian M. Jensen
+*  @license http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3
+*}
+
+{capture name=path}{l s='Swipp payment' mod='swipp'}{/capture}
+{include file="$tpl_dir./breadcrumb.tpl"}
+
+<h2>{l s='Order summary' mod='swipp'}</h2>
+
+{assign var='current_step' value='payment'}
+{include file="$tpl_dir./order-steps.tpl"}
+
+{if $nbProducts <= 0}
+    <p class="warning">{l s='Your shopping cart is empty.'}</p>
+{else}
+    <h3>{l s='Swipp mobile transfer payment' mod='swipp'}</h3>
+    <form action="{$this_path_ssl}validation.php" method="post">
+        <p>
+            <img src="{$this_path}swipp.jpg" alt="{l s='Swipp' mod='swipp'}" width="86" height="49" style="float:left; margin: 0px 10px 5px 0px;" />
+            {l s='You have chosen to pay by swipp mobile transfer.' mod='swipp'}
+            <br/><br />
+            {l s='Here is a short summary of your order:' mod='swipp'}
+        </p>
+        <p style="margin-top:20px;">
+            - {l s='The total amount of your order is' mod='swipp'}
+            <span id="amount" class="price">{displayPrice price=$total currency=$id_currency_accepted}</span>
+            {if $use_taxes == 1}
+                {l s='(tax incl.)' mod='swipp'}
+            {/if}
+        </p>
+        <p>
+            -
+            {l s='We accept the following currency to be sent by swipp mobile transfer:' mod='swipp'}&nbsp;<b>{$name_currency}</b>
+            <input type="hidden" name="currency_payement" value="{$id_currency}" />
+        </p>
+        <p>
+            {l s='Swipp account information will be displayed on the next page.' mod='swipp'}
+            <br /><br />
+            <b>{l s='Please confirm your order by clicking \'I confirm my order\'' mod='swipp'}.</b>
+        </p>
+        <p class="cart_navigation">
+            <a href="{$link->getPageLink('order.php', true)}?step=3" class="button_large hideOnSubmit">{l s='Other payment methods' mod='swipp'}</a>
+            <input type="submit" name="submit" value="{l s='I confirm my order' mod='swipp'}" class="exclusive_large hideOnSubmit" />
+        </p>
+    </form>
+{/if}
